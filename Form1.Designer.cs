d11741ec031e8035cf00fd36ae5b0f6c4c105958
m4rcer <m4rcer@gmail.com>
2023-12-21 03:46:36 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Math_lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart1 = new LiveCharts.Wpf.CartesianChart();
+            this.elementHost2 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart2 = new LiveCharts.Wpf.CartesianChart();
+            this.elementHost3 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart3 = new LiveCharts.Wpf.CartesianChart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(35, 85);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(527, 271);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.cartesianChart1;
+            // 
+            // elementHost2
+            // 
+            this.elementHost2.Location = new System.Drawing.Point(679, 85);
+            this.elementHost2.Name = "elementHost2";
+            this.elementHost2.Size = new System.Drawing.Size(527, 271);
+            this.elementHost2.TabIndex = 0;
+            this.elementHost2.Text = "elementHost1";
+            this.elementHost2.Child = this.cartesianChart2;
+            // 
+            // elementHost3
+            // 
+            this.elementHost3.Location = new System.Drawing.Point(62, 472);
+            this.elementHost3.Name = "elementHost3";
+            this.elementHost3.Size = new System.Drawing.Size(527, 271);
+            this.elementHost3.TabIndex = 0;
+            this.elementHost3.Text = "elementHost1";
+            this.elementHost3.Child = this.cartesianChart3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 434);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Сетка 40 шагов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(780, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Сетка 20 шагов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Сетка 8 шагов";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(324, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "S=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(954, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "S=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(336, 434);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "S=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(359, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(989, 43);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "label7";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(381, 434);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 20);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "label7";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(760, 499);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(503, 20);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Необходимый шаг для получения необходимой точности e=0.01:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(954, 541);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 20);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "label11";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1290, 808);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.elementHost3);
+            this.Controls.Add(this.elementHost2);
+            this.Controls.Add(this.elementHost1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private LiveCharts.Wpf.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Integration.ElementHost elementHost2;
+        private LiveCharts.Wpf.CartesianChart cartesianChart2;
+        private System.Windows.Forms.Integration.ElementHost elementHost3;
+        private LiveCharts.Wpf.CartesianChart cartesianChart3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Math_lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var points1 = GetPoints(8, -Math.PI, Math.PI);
+            var s1 = Simpson(8, -Math.PI, Math.PI);
+
+            var points2 =  GetPoints(20, -Math.PI, Math.PI);
+            var s2 = Simpson(20, -Math.PI, Math.PI);
+
+
+            var points3 = GetPoints(40, -Math.PI, Math.PI);
+            var s3 = Simpson(40, -Math.PI, Math.PI);
+
+            label7.Text = s1.ToString();
+            label8.Text = s2.ToString();
+            label9.Text = s3.ToString();
+
+            string format = "{00.00;-00.00}";
+
+
+            SeriesCollection series = new SeriesCollection(); //отображение данных на график. Линии и т.д.
+            ChartValues<double> zp = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in points1) //Заполняем коллекции
+            {
+                zp.Add(item.Y);
+                date.Add(item.X);
+            }
+            cartesianChart1.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart1.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line.Title = "Y";
+            line.Values = zp;
+
+            series.Add(line); //Добавляем линию на график
+            cartesianChart1.Series = series; //Отрисовываем график в интерфейсе
+
+
+
+            SeriesCollection series2 = new SeriesCollection(); //отображение данных на график. Линии и т.д.
+            ChartValues<double> zp2 = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date2 = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in points2) //Заполняем коллекции
+            {
+                zp2.Add(item.Y);
+                date2.Add(item.X);
+            }
+            cartesianChart2.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart2.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date2.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line2 = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line2.Title = "Y";
+            line2.Values = zp2;
+
+            series2.Add(line2); //Добавляем линию на график
+            cartesianChart2.Series = series2; //Отрисовываем график в интерфейсе
+
+
+
+            SeriesCollection series3 = new SeriesCollection(); //отображение данных на график. Линии и т.д.
+            ChartValues<double> zp3 = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date3 = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in points3) //Заполняем коллекции
+            {
+                zp3.Add(item.Y);
+                date3.Add(item.X);
+            }
+            cartesianChart3.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart3.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date3.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line3 = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line3.Title = "Y";
+            line3.Values = zp3;
+
+            series3.Add(line3); //Добавляем линию на график
+            cartesianChart3.Series = series3; //Отрисовываем график в интерфейсе
+
+
+            var p = 3;
+
+            double e = 0.01;
+
+            double res = 0;
+            decimal h = 1;
+
+            do
+            {
+                decimal length = (decimal)(Math.PI * 2);
+                int n = (int)(length / h/2);
+                int n2 = (int)(length / h);
+
+                var temp = (Simpson(n, -Math.PI, Math.PI) - Simpson(n2, -Math.PI, Math.PI)) / (Math.Pow(2, p) - 1);
+
+                res = Math.Abs((double)temp);
+
+                h /= 2;
+            } while (res >= e);
+
+            label11.Text = h.ToString();
+        }
+
+
+        public static PointF[] GetPoints(int n, double a, double b)
+        {
+            var points = new List<PointF>();
+
+            double h = (b - a) / n;
+            var x = a;
+            while (x <= b)
+            {
+                points.Add(new PointF((float)x, (float)f(x)));
+                x = x + h;
+
+            }
+
+            points.Add(new PointF((float)x, (float)f(x)));
+
+
+            return points.ToArray();
+        }
+
+        public static double Simpson(int n, double a, double b)
+        {
+            double h = (b - a) / n;
+            double s = 0;
+            var x = a + h;
+            while (x < b)
+            {
+                s = s + 4 * f(x);
+                x = x + h;
+                s = s + 2 * f(x);
+                x = x + h;
+            }
+            s = h / 3 * (s +f (a) - f(b));
+
+            return s;
+        }
+
+        /*public static float Simpson(float h, float a, float b)
+        {
+            float s = 0;
+
+            for (float i = a+h; i <= b-h; i += h)
+            {
+                var y = f(i);
+
+                var f0 = f(i-h);
+                var f1 = f(i);
+                var f2 = f(i+h);
+
+                var res = (h / 3) * (f0 + 4 * f1 + f2);
+
+                s += res;
+            }
+
+            return s;
+        }*/
+
+        public static double f(double x)
+        {
+            var sin = Math.Sin(x);
+            var sin2 = -Math.Pow(sin, 2);
+
+            var res = Math.Pow(Math.E, sin2);
+
+            return res;
+        }
+    }
+}
